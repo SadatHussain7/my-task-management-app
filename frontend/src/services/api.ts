fc6e3768362b947e26d47ddf0d4480be958be729
Sadat <sadat.bagban@intimetec.com>
2024-06-25 16:52:04 +0530

    Updated the base url from env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4004/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const getTasks = () => api.get("/tasks");
